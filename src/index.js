f750129e73cbc4983f85c17ea82d5f7dba9e341a
Marvin Frede <mfrede@slashwhy.de>
2023-08-21 17:02:33 +0200

    use subscription (without error)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, gql, split } from '@apollo/client';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { createClient } from 'graphql-ws';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -17,8 +20,31 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-const client = new ApolloClient({
+const httpLink = new HttpLink({
   uri: 'https://localhost:5001/graphql',
+});
+
+const wsLink = new GraphQLWsLink(createClient({ url: 'wss://localhost:5001/graphql' }));
+
+// The split function takes three parameters:
+//
+// * A function that's called for each operation to execute
+// * The Link to use for an operation if the function returns a "truthy" value
+// * The Link to use for an operation if the function returns a "falsy" value
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+const client = new ApolloClient({
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
@@ -34,4 +60,20 @@ client
     `,
     errorPolicy: 'all'
   })
-  .then((result) => console.log(result));
+  .then((result) => console.log('query result :', result))
+  .catch((error) => console.log('query error: ', error));
+
+client
+  .subscribe({
+    query: gql`
+      subscription {
+        alienChanged {
+          name,
+        }
+      }
+    `,
+    errorPolicy: 'all'
+  }).subscribe({
+    next: (result) => console.log('subscription result :', result),
+    error: (error) => console.log('subscription error: ', error),
+  });
